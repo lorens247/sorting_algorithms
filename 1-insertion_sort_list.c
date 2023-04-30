7bd00aaa21e47c24ce591719eae4e84a5bbbcb18
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-04-30 09:46:15 -0700

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,16 +11,16 @@
 void swap(listint_t *a, listint_t *b)
 {
     /* Update the next and previous pointers of a and b's neighbors */
-    if (a->prev)
-        a->prev->next = b;
-    if (b->next)
-        b->next->prev = a;
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
 
     /* Swap the next and previous pointers of a and b */
-    a->next = b->next;
-    b->prev = a->prev;
-    a->prev = b;
-    b->next = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 }
 
 /**
@@ -31,37 +31,35 @@ void swap(listint_t *a, listint_t *b)
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *i, *j;
+	listint_t *i, *j;
 
     /* Check if the list exists and has at least 2 nodes */
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
     /* Iterate over the list */
-    i = (*list)->next;
-    while (i)
-    {
-        j = i;
-        i = i->next;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+	/* Move the node at j to its correct position */
+	while (j && j->prev)
+	{
+	/* If the previous node is greater than the current node, swap them */
+	if (j->prev->n > j->n)
+		{
+		    swap(j->prev, j);
+		/* Update the head of the list if necessary */
+		if (!j->prev)
+			*list = j;
 
-        /* Move the node at j to its correct position */
-        while (j && j->prev)
-        {
-            /* If the previous node is greater than the current node, swap them */
-            if (j->prev->n > j->n)
-            {
-                swap(j->prev, j);
-                
-                /* Update the head of the list if necessary */
-                if (!j->prev)
-                    *list = j;
-
-                /* Print the list after each swap */
-                print_list((const listint_t *)*list);
-            }
-            else
-                j = j->prev;
-        }
+		/* Print the list after each swap */
+	       print_list((const listint_t *)*list);
+		}
+	else
+		    j = j->prev;
+	}
 }
 }
 
